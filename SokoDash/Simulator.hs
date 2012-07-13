09b8d61c99f6cd3d7270cab1bc0f6c1254e3d1a7
Dr. ERDI Gergo <gergo@erdi.hu>
2012-07-13 23:23:17 +0800

    processInput

--- SokoDash/Simulator.hs
+++ SokoDash/Simulator.hs
@@ -1,14 +1,59 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TupleSections #-}
-module SokoDash.Simulator (simulate) where
+module SokoDash.Simulator
+       ( Dir(..), Input(..)
+       , InputResult(..), processInput
+       , simulate
+       ) where
 
 import SokoDash.World
 
+import Prelude hiding (Left, Right)
+
 import Data.Ix
-import Data.Array ((!))
+import Data.Array ((!), (//))
 import qualified Data.Array as Array
 import Control.Arrow (second, (&&&))
 import Data.Maybe (mapMaybe)
+
+import Debug.Trace
+
+data Dir = Left | Right | Up | Down
+data Input = Dir Dir | Wait
+
+moveDir :: Dir -> (Int, Int) -> (Int, Int)
+moveDir d (x, y) = (x + dx, y + dy)
+  where
+    (dx, dy) = case d of
+        Left  -> (-1, 0)
+        Right -> (1, 0)
+        Up    -> (0, -1)
+        Down  -> (0, 1)
+
+data InputResult = InvalidInput | NewState State | Finished
+
+processInput :: Input -> State -> InputResult
+processInput input s@State{..} = case input of
+    Wait -> NewState s
+    Dir dir -> case stateWorld!pos' of
+        Empty -> NewState $ move s
+        Earth -> NewState $ clear . move $ s
+        Lambda -> NewState $ collect . clear . move $ s
+        LambdaLift | stateLambdaRemaining == 0 -> Finished
+        Rock -> case dir of
+            Left | stateWorld!pos'' == Empty -> NewState $ push . clear . move $ s
+            Right | stateWorld!pos'' == Empty -> NewState $ push . clear . move $ s
+            _ -> InvalidInput
+        _ -> InvalidInput
+      where
+        pos' = moveDir dir statePos
+        pos'' = moveDir dir pos'
+        move s = s{ statePos = pos' }
+        clear s = s{ stateWorld = stateWorld // [(pos', Empty)] }
+        collect s = s{ stateLambdaRemaining = pred stateLambdaRemaining
+                     , stateLambdaCollected = succ stateLambdaCollected
+                     }
+        push s = s{ stateWorld = stateWorld // [(pos'', Rock)] }
 
 simulate :: State -> State
 simulate s@State{..} = s{ stateWorld = simulateWorld statePos stateWorld }
