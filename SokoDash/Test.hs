a5640f1854e18ca0dd910b20a940846d65618a80
Dr. ERDI Gergo <gergo@erdi.hu>
2012-07-13 23:23:25 +0800

    main loop

--- SokoDash/Test.hs
+++ SokoDash/Test.hs
@@ -1,12 +1,32 @@
 module SokoDash.Test where
 
+import Prelude hiding (Left, Right)
 import SokoDash.World
+import SokoDash.Simulator
 
 import Control.Applicative
 import System.IO
 import System.Environment
 
+import Data.Function (fix)
+import Data.Char (toUpper)
+
 main = do
     [infile] <- getArgs
     s <- parse <$> readFile infile
-    print s
+
+    flip fix s $ \loop s -> do
+        print s
+        inp <- getLine
+        print inp
+        let input = case map toUpper inp of
+                "W" -> Dir Up
+                "A" -> Dir Left
+                "S" -> Dir Down
+                "D" -> Dir Right
+                _ -> Wait
+        case processInput input s of
+            NewState s' -> loop $ simulate s'
+            Finished -> putStrLn "Finished"
+            InvalidInput -> putStrLn "You can't get ye flask!" >> loop s
+
